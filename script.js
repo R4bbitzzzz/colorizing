db5207ce594425ec75ab97ca25bdb55c307e7c2f
R4bbitzzzz <agostonpalatinus@gmail.com>
2023-11-10 13:55:41 +0100

    nigga

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const button = document.querySelector("button");
-const circle = document.querySelector(".circle");
+
 
 button.onclick = (event) = {
-
+    const circle = document.querySelector(".circle");
     const red = document.querySelector("input:first-child");
     const green = document.querySelector("input:nth-child(2)");
     const blue = document.querySelector("input:last-of-type");
