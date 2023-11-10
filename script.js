5f30517c469e8cd7e8e75d76bfc2fa67509b3ee7
R4bbitzzzz <agostonpalatinus@gmail.com>
2023-11-10 14:06:04 +0100

    nem egy kozepre de mukszik am upodate

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const button = document.querySelector("button");
 
 
-button.onclick = (event) = {
+button.onclick = () => {
     const circle = document.querySelector(".circle");
     const red = document.querySelector("input:first-child");
     const green = document.querySelector("input:nth-child(2)");
     const blue = document.querySelector("input:last-of-type");
     
-}
+    circle.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`
+};
