1db7bc085169bd438c7bc6ed6e4fcf7761254fc7
R4bbitzzzz <agostonpalatinus@gmail.com>
2023-11-10 13:55:09 +0100

    sadadsdadadadasdadadadasdasdasdasdasdaa

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const button = document.querySelector("button")
-const circle = document.querySelector(".circle")
+const button = document.querySelector("button");
+const circle = document.querySelector(".circle");
+
+button.onclick = (event) = {
+
+    const red = document.querySelector("input:first-child");
+    const green = document.querySelector("input:nth-child(2)");
+    const blue = document.querySelector("input:last-of-type");
+    
+}
